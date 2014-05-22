d673862b5ed736395ff5f272822b24fac9592858
Marcus Kazmierczak <marcus@mkaz.com>
2014-05-22 10:39:25 -0700

    tweak fswatch to use watchdog package, remove GUI

--- README.md
+++ README.md
@@ -1,23 +1,12 @@
 fswatch
 =======
 
-This script will watch a local directory using Mac OS X FSEvents
-and on change will sync to a remote directory. The script can be
-easily modified to do whatever you want on a change event.
+This script will watch a local directory and on change will sync to a remote
+directory. The script can be easily modified to do whatever you want on a change
+event.
 
 **Install** 
-`pip install fsevents`
+
+`pip install watchdog`
 
 
-**Note:** if you are running against a large directory, it will 
-take awhile at the beginning, my hunch is it needs to traverse
-all sub-directories and attach the listeners everywhere
-
-
-### TODO
-
-  * setup as a daemon
-  * add catch-up mode, in case weren't running it and made changes
-  
- 
- 
--- fswatch.py
+++ fswatch.py
@@ -3,73 +3,80 @@ fswatch.py
 Marcus Kazmierczak, marcus@mkaz.com
 http://github.com/mkaz/fswatch/
 
-This script will watch a local directory using Mac OS X FSEvents
-and on change will sync to a remote directory. The script can be
-easily modified to do whatever you want on a change event.
+This script will watch a local directory using and on change will 
+sync to a remote directory. The script can be easily modified to 
+do whatever you want on a change event.
 
-requires: pip install fsevents
-
-Note: if you are running against a large directory, it will 
-take awhile at the beginning, my hunch is it needs to traverse
-all sub-directories and attach the listeners everywhere
+requires: pip install watchdog
     
 """
-import os, datetime, time       # python packages
-import fsevents                 # https://pypi.python.org/pypi/MacFSEvents
-import Tkinter
 
-# CONFIG PARAMS, set envirovnment variables or hardcode
-# include trailing slashes for rsync, being more explicit is better
-local_path  = os.getenv('FSWATCH_LOCAL_PATH',  '/hard/coded/path/')
-remote_host = os.getenv('FSWATCH_REMOTE_HOST', 'user@remote.server.com')
-remote_path = os.getenv('FSWATCH_REMOTE_PATH', '/remote/hard/coded/')
+import os, os.path, datetime, time
+import ConfigParser
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+# config parameters
+local_path = ""
+remote_host = ""
+remote_path = ""
 
 # list of files to ignore, simple substring match
 ignore_list = ['.svn', '.DS_Store', '.git']
 
+
+def load_config():
+    global local_path, remote_host, remote_path
+    configParser = ConfigParser.RawConfigParser()   
+    configFilePath = "./fswatch.conf"
+    if not os.path.isfile(configFilePath):
+        print("Config file fswatch.conf not found")
+        os.exit(1)
+
+    configParser.read(configFilePath)
+    local_path  = configParser.get('local', 'path')
+    remote_host = configParser.get('remote', 'server')    
+    remote_path = configParser.get('remote', 'path')
+
+        
 def display(str):
-    global ta
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    mystr = "[{0}] {1} \n".format(now, str)
-    ta.insert(Tkinter.END, mystr)
+    print( "[{0}] {1} \n".format(now, str) )
 
 
-def file_event_sync(event):
-    """ callback on event action, this does the sync passing in filename """
-    filename = event.name
-    remote_file = filename.replace(local_path, '')  # switch local path to remote
-    for ig in ignore_list:                          # check ignore list
-        if ig in filename:
-            return
-    
-    # basic rsync to remote server
-    cmd = " rsync -cazq --del %s %s:%s%s " % (filename, remote_host, remote_path, remote_file)
-    display("Syncing %s " % filename)
-    os.system(cmd)
+# handles sync event actions, only modified 
+class MySyncHandler(FileSystemEventHandler):
+
+    def on_modified(self, event):
+        global local_path, remote_host, remote_path
+        filename = event.src_path
+        remote_file = filename.replace(local_path, '')  # switch local path to remote
+        for ig in ignore_list:                          # check ignore list
+            if ig in filename:
+                return
+
+        # basic rsync to remote server
+        cmd = " rsync -cazq --del %s %s:%s%s " % (filename, remote_host, remote_path, remote_file)
+        display("Syncing %s " % filename)
+        os.system(cmd)
 
 
-## Main
+## main loop
+def main():
+    global local_path, remote_host, remote_path
+    load_config();
 
-## Setup GUI
-canvas = Tkinter.Tk()
-scroll = Tkinter.Scrollbar(canvas)
-ta = Tkinter.Text(canvas)
+    observer = Observer()
+    observer.schedule(MySyncHandler(), ".", recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
-scroll.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
-ta.pack(side=Tkinter.LEFT, fill=Tkinter.Y)
-scroll.config(command=ta.yview)
-ta.config(yscrollcommand=scroll.set)
 
-## Setup Watcher
-observer = fsevents.Observer()
-observer.start()
-stream = fsevents.Stream(file_event_sync, local_path, file_events=True)
-observer.schedule(stream)
-
-## running
-display("Watching: %s " % local_path)
-canvas.mainloop()
-
-## clean-up
-observer.unschedule(stream)
-observer.stop()
+if __name__ == "__main__":
+    main()
